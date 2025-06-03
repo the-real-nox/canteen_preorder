665121356afa76354a7a6743afffa4ea64c07551
the-real-nox <the-real-nox@outlook.com>
2025-06-03 09:07:48 +0200

    Added file left out from last commit.

--- src/client/login-page/LoginPage.tsx
+++ src/client/login-page/LoginPage.tsx
@@ -1,7 +1,6 @@
 import logo from '../img/HTL_Kaindorf_Logo.png'
 import Image from "react-bootstrap/Image";
 import "../css/login-page.css";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
