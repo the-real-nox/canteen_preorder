df284d11b2bf64c7582e5aba41b3446dbcdef58a
the-real-nox <the-real-nox@outlook.com>
2025-06-10 09:29:52 +0200

    Fixed student-page displaying on staff.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,6 +3,7 @@ import {LoginPage} from "./login-page/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StudentPage } from "./student-page/StudentPage";
 import "bootstrap/dist/css/bootstrap.css";
+import { StaffPage } from "./staff/Staff";
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
           <Router>
               <Routes>
                   <Route path="/login" element={<LoginPage />} />
-                  <Route path="/staff" element={<StudentPage />} />
+                  <Route path="/staff" element={<StaffPage />} />
                   <Route path="/student" element={<StudentPage />} />
               </Routes>
           </Router>
