a4c725347d10de2d7c9ffe54eb736671d3af56a3
the-real-nox <the-real-nox@outlook.com>
2025-06-17 08:52:33 +0200

    Added registration page.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import {LoginPage} from "./login-page/LoginPage";
+import {LoginPage} from "./login/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StudentPage } from "./student-page/StudentPage";
 import "bootstrap/dist/css/bootstrap.css";
 import { StaffPage } from "./staff/Staff";
+import { Registration } from "./registration/Registration";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div>
           <Router>
               <Routes>
+                  <Route path="/reg" element={<Registration />} />
                   <Route path="/login" element={<LoginPage />} />
                   <Route path="/staff" element={<StaffPage />} />
                   <Route path="/student" element={<StudentPage />} />
---
+++ src/client/registration/Registration.tsx
@@ -0,0 +1,34 @@
+import { Form } from "react-bootstrap";
+import { Button } from "../components/Button";
+
+export function Registration() {
+    return (
+        <div className="vw-100 vh-100 d-flex justify-content-center align-items-center">
+            <Form className="border p-3 rounded">
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control type="email" placeholder="First-name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control type="email" placeholder="Last-name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept terms of use" required/>
+                </Form.Group>
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
